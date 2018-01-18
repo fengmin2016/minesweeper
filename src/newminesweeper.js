b4d05f767e8e9d35bd271c333752f1271caac32a
Ze <Ze@main-10-231-24-111.tamulink.tamu.edu>
2018-01-18 10:18:20 -0600

    first commit

---
+++ src/newminesweeper.js
@@ -0,0 +1,43 @@
+const mineSweeperBoard = (row, column)=>{
+  const board = [];
+  for(rowIndex = 0; rowIndex <row; rowIndex ++){
+
+    const rowArray = [];
+    for(columnIndex = 0; columnIndex<column; columnIndex ++){
+      rowArray.push(' ');
+    }
+    board.push(rowArray);
+  }
+  return board;
+}
+
+const mineSweeperBombBoard = (row, column, bomb)=>{
+  const board = [];
+  for(rowIndex = 0; rowIndex <row; rowIndex ++){
+
+    const rowArray = [];
+    for(columnIndex = 0; columnIndex<column; columnIndex ++){
+      rowArray.push(null);
+    }
+    board.push(rowArray);
+  }
+
+
+for(let numberBombsPlaced = 0; numberBombsPlaced < bomb;  numberBombsPlaced++ ){
+  BombRowIndex = Math.floor(Math.random()*row);
+  BombColumnIndex = Math.floor(Math.random()*column);
+  board[BombRowIndex][BombColumnIndex]= 'B';
+}
+  return board;
+}
+
+const printBoard = (board)=>{
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+}
+
+
+
+
+//console.log(mineSweeperBoard(4,3));
+//console.log(mineSweeperBombBoard(4,3,2));
+printBoard(mineSweeperBombBoard(4,3,2));
